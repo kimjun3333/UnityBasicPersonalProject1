4692e25ea002e1a1973edf7671de70ca8942a6c5
Kim Jun Hyeong <kheami35@gmail.com>
2025-07-23 16:27:55 +0900

    Feat_Player Anim
    플레이어 상하좌우 애니메이션 추가

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     Rigidbody2D rigid;
+    
+    public float speed = 2f;
 
-    public float speed = 1f;
+    Animator anim;
+
+    bool isHorizonMove;
 
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -19,13 +24,25 @@ public class Player : MonoBehaviour
         Move();
     }
 
-    private void Move()
+    private void Move() //이동 로직꾸
     {
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
 
-
         rigid.velocity = new Vector2(h, v) * speed;
 
+
+        if (v < 0)
+            anim.SetInteger("Move", 0);
+        else if (v > 0)
+            anim.SetInteger("Move", 1);
+        else if (h < 0)
+            anim.SetInteger("Move", 2);
+        else if (h > 0)
+            anim.SetInteger("Move", 3);
+
+        
     }
+
+
 }
